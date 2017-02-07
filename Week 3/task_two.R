f1d02ec4e3151113eeac5f7f5929411dcf58b1fb
Rozran <rozran00@gmail.com>
2017-02-06 19:30:11 -0500

    In class exercise

---
+++ Week 3/task_two.R
@@ -0,0 +1,40 @@
+#####################
+## DATE: 2/6/2017  ##
+## TASK 2          ##
+#####################
+
+filename <- paste("~/Desktop/MAT 7500 - Statistical Programming/Week 2/prac2.", 
+                  "txt", sep = "")
+colDat <- read.table(filename, skip = 1, header = TRUE) ## SKIP THE FIRST LINE
+                                                        ## NEXT LINE IS HEADERS
+
+## USING VECTOR MANIPULATION
+colDat$type_of_person <- ifelse(is.na(colDat$Verbal.SAT) | is.na(colDat$Math.SAT), 
+                                ifelse(is.na(colDat$Verbal.SAT) & is.na(colDat$Math.SAT), 
+                                       "Missing Math & Verbal Score", 
+                                       ifelse(is.na(colDat$Verbal.SAT) & !is.na(colDat$Math.SAT), 
+                                              "Missing Verbal Score", "Missing Math Score")),
+                                ifelse(colDat$Verbal.SAT == colDat$Math.SAT, "Equal", 
+                                ifelse(colDat$Verbal.SAT > colDat$Math.SAT, 
+                                       "Verbal", "Math")))
+
+## USING A FOR LOOP
+for(i in 1:dim(colDat)[1]){
+        colDat$type_of_person_2[i] <- if(is.na(colDat$Verbal.SAT[i]) | is.na(colDat$Math.SAT[i])){
+                if(is.na(colDat$Verbal.SAT[i]) & is.na(colDat$Math.SAT[i])){
+                        "Missing Math & Verbal Score"
+                } else if (is.na(colDat$Verbal.SAT[i]) & !is.na(colDat$Math.SAT[i])){
+                        "Missing Verbal Score"
+                } else {
+                        "Missing Math Score"
+                }
+        } else {
+                if(colDat$Verbal.SAT[i] == colDat$Math.SAT[i]){
+                        "Equal"
+                } else if(colDat$Verbal.SAT[i] > colDat$Math.SAT[i]) {
+                        "Verbal"
+                } else {
+                        "Math"
+                }
+        }
+}
